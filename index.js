63220d83dc6b93d1e953507bc27a3ac5548d0f57
shaharul siyam <shaharulsiyam0273@.com>
2023-11-08 09:57:35 +0600

    delete request and update status completed

--- index.js
+++ index.js
@@ -100,16 +100,16 @@ async function run() {
       }
     })
     app.delete('/foods', verifyToken, async (req, res) => {
-      const {id}=req.query;
-       const queary = {_id : new ObjectId(id)}
-       const query = { foodid : id}
-        const result = await Foods.deleteOne(queary)
-        const deletefromrequest = await Foodsrequest.deleteOne(query)
-        res.send(result)
+      const { id } = req.query;
+      const queary = { _id: new ObjectId(id) }
+      const query = { foodid: id }
+      const result = await Foods.deleteOne(queary)
+      const deletefromrequest = await Foodsrequest.deleteOne(query)
+      res.send(result)
 
     })
     app.put('/foods', verifyToken, upload.single('file'), async (req, res) => {
-      const {id} = req.query;
+      const { id } = req.query;
       const updateData = req.body;
       const foodimage = req.file ? `${req.file.destination}${req.file.filename}` : null;
       if (req.user.email !== updateData.useremail) {
@@ -125,36 +125,55 @@ async function run() {
       const result = await Foods.updateOne(queary, update);
       res.send(result)
     })
+
     app.post('/foodrequest', verifyToken, async (req, res) => {
       const data = req.body
       const result = await Foodsrequest.insertOne(data)
       res.send(result)
     })
-    app.get('/foodrequest', verifyToken, async (req, res) => {
-      const {id,email} = req.query;
+    app.delete('/foodrequest', verifyToken, async (req, res) => {
+      const { foodId, email, requester } = req.query;
       if (!req.user.email === email) {
         return res.status(403).send({ message: 'forbidden access' })
       } else {
-        const query = { useremail: email ,foodid : id}
+        const update = {
+          $set: {
+            status : 'Deliverd'
+          },
+        };
+        const query = { useremail: email, foodid: foodId, requestUser: requester }
+        const updatequery = {_id : new ObjectId(foodId)}
+        const updates = await Foods.updateOne(updatequery,updates)
+        const result = await Foodsrequest.deleteOne(query)
+        return res.send(result)
+      }
+    })
+    app.get('/foodrequest', verifyToken, async (req, res) => {
+      const { id, email } = req.query;
+      if (!req.user.email === email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      } else {
+        const query = { useremail: email, foodid: id }
         const result = await Foodsrequest.find(query).toArray()
         const modifiedData = result.map(item => ({
           ...item,
           foodimage: item.foodimage.includes('http') ? item.foodimage : `http://${location}/${item.foodimage}`,
-        }));    
+        }));
         return res.send(modifiedData)
       }
     })
     app.get('/managefood', verifyToken, async (req, res) => {
-      const {id,email} = req.query;
+      const location = req.get('host')
+      const { id, email } = req.query;
       if (!req.user.email === email) {
         return res.status(403).send({ message: 'forbidden access' })
       } else {
-        const query = { useremail: email ,_id : new ObjectId(id)}
+        const query = { useremail: email, _id: new ObjectId(id) }
         const result = await Foods.findOne(query)
-        const modifiedData ={
+        const modifiedData = {
           ...result,
           foodimage: result.foodimage.includes('http') ? result.foodimage : `http://${location}/${result.foodimage}`,
-        };        
+        };
         return res.send(modifiedData)
       }
     })
@@ -171,7 +190,7 @@ async function run() {
         const modifiedData = result.map(item => ({
           ...item,
           foodimage: item.foodimage.includes('http') ? item.foodimage : `http://${location}/${item.foodimage}`,
-        }));        
+        }));
         return res.send(modifiedData)
       }
     })
